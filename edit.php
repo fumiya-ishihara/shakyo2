15cee7dbec5b8c6de57484ff592da77d8278cb6f
“fumiya-ishihara” <fishihara41@gmail.com>
2023-03-29 00:09:37 +0900

    1行追加

--- edit.php
+++ edit.php
@@ -3,10 +3,13 @@
 <div class="th1">商品名</div>
 <div class="th1">価格</div><br>
 <form action="edit3.php" method="post">
-<input type="hidden" name="command" value="insert">
-<div class="td0"></div>
-<div class="td1"><input type="text" name="name"></div>
-<div class="td1"><input type="text" name="price"></div>
-<div class="td2"><input type="submit" value="追加"></div>
+  <input type="hidden" name="command" value="insert">
+  <div class="td0"></div>
+  <div class="td1"><input type="text" name="name"></div>
+  <div class="td1"><input type="text" name="price"></div>
+  <div class="td2"><input type="submit" value="追加"></div>
 </form><br>
 <?php require '../footer.php'; ?>
+
+
+<?php echo '一行追加' ?>
